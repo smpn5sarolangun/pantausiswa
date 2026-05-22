d16fc1981baec046263306948f297c1f5992d5a7
smpn5sarolangun <smpn5sarolangun@gmail.com>
2026-05-22 17:46:54 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'pantau-siswa-v1';
+const ASSETS = [
+  './',
+  './pantau-siswa.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network first untuk API, cache first untuk assets
+  if (e.request.url.includes('script.google.com')) {
+    e.respondWith(fetch(e.request).catch(() => new Response('{"error":"offline"}', {headers:{'Content-Type':'application/json'}})));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
